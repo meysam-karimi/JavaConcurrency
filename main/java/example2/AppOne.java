246ff7e188b8646fd07ba7ac3748e96d801c28c8
Meysam <karimi.ebox@yahoo.com>
2020-11-21 17:50:27 +0330

    sleep in thread execution

--- main/java/example2/AppOne.java
+++ main/java/example2/AppOne.java
@@ -1,4 +1,22 @@
 package example2;
 
+import java.util.concurrent.TimeUnit;
+
 public class AppOne {
+    public static void main(String[] args) {
+        Runnable runnable = () -> {
+            try {
+                String name = Thread.currentThread().getName();
+                System.out.println("Foo " + name);
+                TimeUnit.SECONDS.sleep(2);
+                System.out.println("Bar " + name);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+        thread.start();
+    }
 }
