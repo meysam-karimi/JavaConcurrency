1ff6d13c9db73d2b6027dd9d254ea9e3ffcbdf0e
Meysam <karimi.ebox@yahoo.com>
2020-12-01 17:57:10 +0330

    timeOut example

---
+++ main/java/example8/App8.java
@@ -0,0 +1,27 @@
+package example8;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.*;
+
+public class App8 {
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executerService = Executors.newWorkStealingPool();
+        List<Callable<String>> callables = Arrays.asList(() -> "task1", () -> "task2");
+
+        executerService.invokeAll(callables)
+                .stream()
+                .map(stringFuture -> {
+                    try {
+                        return stringFuture.get();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                    return null;
+                }).forEach(System.out::println);
+
+
+    }
+}
